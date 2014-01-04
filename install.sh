f2df3b0117bb6dd5e71111aca0b67b231367e2df
novalore <novalore@19.coop>
2014-01-04 12:46:04 +0100

    installer update

--- install.sh
+++ install.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-############################
 # install.sh
 # Install script to symlink config files in ~/dotfiles to "canonical" homedir-based locations
-############################
 
-########## Variables
-
+# Vars:
 # dotfiles directory
 dir=~/dotfiles
 # old dotfiles backup directory
@@ -13,9 +10,9 @@ olddir=~/dotfiles_old
 # list of files (even folders if needed) to symlink in homedir
 files="bash_aliases vimrc.local vimrc.bundles.local"
 
-##########
+# Installation process:
 
-# create dotfiles_old in homedir
+# create dotfiles_bck in homedir
 echo "Creating $olddir for backup purposes"
 mkdir -p $olddir
 echo "...done"
@@ -25,10 +22,12 @@ echo "Changing to the $dir directory"
 cd $dir
 echo "...done"
 
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
+# move any existing dotfiles in homedir to dotfiles_bck directory, then create symlinks
 for file in $files; do
     echo "Moving any existing dotfiles from ~ to $olddir"
     mv ~/.$file ~/dotfiles_old/
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
 done
+
+#Once controlled, you can simply delete dotfiles_bck, if you like...
