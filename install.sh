4f30bb8a18c640524c7d4db539606a37467825b5
faust <faust@19.coop>
2014-02-17 13:16:49 +0100

    mutt: let's start...

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ dir=~/dotfiles
 # old dotfiles backup directory
 olddir=~/dotfiles_old
 # list of files (even folders if needed) to symlink in homedir
-files="bashrc bash_aliases vimrc.local vimrc.before.local vimrc.bundles.local"
+files="bashrc bash_aliases vimrc.local vimrc.before.local vimrc.bundles.local offlineimaprc mutt muttrc"
 
 # Installation process:
 
